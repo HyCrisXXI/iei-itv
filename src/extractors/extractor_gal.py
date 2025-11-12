1dcb6fcb54b2aa8eab0488447ac8ee83efe01721
HyCrisXXI <96534422+HyCrisXXI@users.noreply.github.com>
2025-11-12 13:39:11 +0100

    Update extractor_gal.py

--- src/extractors/extractor_gal.py
+++ src/extractors/extractor_gal.py
@@ -1,12 +1,1 @@
 # src/extractors/extractor_gal.py
-import csv
-import json
-
-def csvtojson():
-    with open("Estacions_ITV.csv", "r", encoding="ISO-8859-1") as csvfile, \
-            open("estaciones_gal.json", "w", encoding="utf8") as jsonfile:
-        reader = csv.DictReader(csvfile, delimiter=';')
-        data = [row for row in reader]
-
-        json.dump(data, jsonfile, ensure_ascii=False, indent=4)
-csvtojson()
