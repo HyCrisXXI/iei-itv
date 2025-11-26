f26a06adcf8ad6ce352e12ce1f480ff78974f1e4
HyCrisXXI <96534422+HyCrisXXI@users.noreply.github.com>
2025-11-26 11:39:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.database.session import create_db_and_tables
 from src.extractors.extractor_gal import  transformed_data_to_database as gal_to_db
 from src.extractors.extractor_cat import  transformed_data_to_database as cat_to_db
 from src.extractors.extractor_cv import   insert_transformed_to_db as cv_to_db
+from src.extractors.extractor_cv import  jsontojson
 
 
 def startup():
@@ -14,8 +15,9 @@ def startup():
     gal_to_db()
     print("Subiendo datos de ITV-CAT.xml a la BD.")
     cat_to_db()
-    print("Subiendo datos de ITV-CAT.xml a la BD.")
-    cv_to_db()
+    print("Subiendo datos de estaciones SITVAL a la BD.")
+    cv_raw = jsontojson()
+    cv_to_db(cv_raw)
 
 if __name__ == "__main__":
     startup()
